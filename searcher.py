725debb0f6815be054b02d2c4816161de668ab57
Antt02 <antoniolopez18022002@gmail.com>
2023-06-16 23:10:30 +0200

    dijkstra working after refactor

--- searcher.py
+++ searcher.py
@@ -7,28 +7,28 @@ import heapq
 import os
 import graphviz
 
-
 # Global vars needed after
 IPs = set()
 routersInfo = dict()
 routersIfs = dict()
+routersIps = dict()
 routersExtIps = dict()
 shortest_paths = dict()
 
-
 """ Method used to retrieve all necessary info from the routers """
+
+
 class IntfPointingIps:
     def __init__(self, intfIP, pointingIps):
         self.intfIp = intfIP
         self.pointingIps = pointingIps
 
+
 class IntfProperties:
-    def __init__(self, intfIP, netmask,speed):
+    def __init__(self, intfIP, netmask, speed):
         self.intfIp = intfIP
         self.netmask = netmask
-        self.speed =speed
-
-
+        self.speed = speed
 
 
 def recursiveSearch(sessionIp):
@@ -50,10 +50,11 @@ def recursiveSearch(sessionIp):
 
     routerIfs = list()
     routerPairExtIps = list()
-
+    routerIps = list()
     # Getting the information of all interfaces
     for entry in description:
         intExtIps = list()
+
         if entry.oid == 'ifPhysAddress' and entry.value != '':  # Since physical address -> no loopbacks
             index = entry.oid_index
             mac = entry.value
@@ -73,18 +74,19 @@ def recursiveSearch(sessionIp):
                         speed = session.get('1.3.6.1.2.1.2.2.1.5.' + index)
                         routerIfs.append(IntfProperties(ip, netmask.value, speed.value))
                         intIp = ip
+                        routerIps.append(ip)
                         IPs.add(ip)
                     else:
                         intExtIps.append(ip)
                         print("Connected to: " + ip)
                         extIPs.add(ip)
                 # Save connected IPs' pair
-
                 routerPairExtIps.append(IntfPointingIps(intIp, intExtIps))
                 print()
     # Save router's info in global vars
     routersIfs[name] = routerIfs
     routersExtIps[name] = routerPairExtIps
+    routersIps[name] = routerIps
     # Next iteration (or end)
     for ip in extIPs:
         if ip not in IPs:
@@ -100,17 +102,22 @@ def dijkstra(routers):
     router_if = defaultdict(list)
     shortest_paths = dict()
     # Get all IPs
-    for router1, router2 in routers:
-        router1ips = getIps(router1, routersIfs)
-        router2ips = getIps(router2, routersIfs)
+    for router in routers:
+        router1ips = getIps(router)
         all_ips.update(router1ips)
-        all_ips.update(router2ips)
-        # Save adjacent routers' IPs as connected (distance = 1)
-        for ip1 in router1ips:
-            for ip2 in router2ips:
-                router_if[ip1].append(ip2)
-                router_if[ip2].append(ip1)
+        ips2=list()
+        for connectedIps in routers[router]:
+            for ip2 in connectedIps.pointingIps:
+                ips2.append(ip2)
+                # Save adjacent routers' IPs as connected (distance = 1)
+                for ip1 in router1ips:
+                    if ip2 not in router_if[ip1]:
+                        router_if[ip1].append(ip2)
+                    if ip1 not in router_if[ip2]:
+                        router_if[ip2].append(ip1)
+        all_ips.update(ips2)
     # Use Dijkstra's algorithm to get the shortest paths between all the IPs
+    print(all_ips)
     for source in all_ips:
         shortest_paths[source] = {}
         visited = set()
@@ -131,36 +138,13 @@ def dijkstra(routers):
                     heapq.heappush(heap, (distance, neighbor_ip))
                     shortest_paths[source][neighbor_ip] = current_ip
     return shortest_paths
-    """ NO BORRAR: tinc mig apanyat el formateig del missatge com el vol el César pero per a una altra versió que mirava 
-        routers no ip's, potser l'utilitzo si no li deixem talqual amb les ip's """
-    """
-    distances = {node: float('inf') for node in graph}
-    distances[source] = 0
-
-    routes = {node: [] for node in graph}
-    queue = [(0, source, [])]
-    while queue:
-        dist, node, route = heapq.heappop(queue)
-        if dist > distances[node]:
-            continue
-        for neighbor in graph[node]:
-            new_dist = dist + graph[node][neighbor]
-            if new_dist < distances[neighbor]:
-                distances[neighbor] = new_dist
-                new_route = route + [neighbor]
-                routes[neighbor] = new_route
-                heapq.heappush(queue, (new_dist, neighbor, new_route))
-
-    return distances, routes
-    """
 
 
-""" Method used to collect all ip's associated with every router """
+""" Method used to collect all ip's associated with a router """
 
 
-def getIps(router, allrouters):
-    ips = [pair[0] for pair in allrouters[router]]
-    return ips
+def getIps(router):
+    return routersIps[router]
 
 
 def printTrap():
@@ -177,6 +161,7 @@ def waitForTrap(notifier):
         if "IN_MODIFY" in type_names and path == "/etc/snmp/script/logs.txt":
             printTrap()
 
+
 def createGraph():
     edges = []
     net = graphviz.Digraph(filename="net.gv", comment='Network layout')
@@ -188,67 +173,72 @@ def createGraph():
     for routerId in routersExtIps.keys():
         intfs = routersExtIps[routerId]
         for intf in intfs:
-            if(len(intf.pointingIps)>1):
-                speed=0
+            if (len(intf.pointingIps) > 1):
+                speed = 0
                 for extRouter in routersIfs.items():
                     for item in extRouter[1]:
                         if item.intfIp == intf.intfIp:
-                            speed=item.speed
-                foundIp= False
+                            speed = item.speed
+                foundIp = False
                 for switch, ips in switches.items():
                     if intf.intfIp in ips:
                         foundIp = True
-                        net.edge(routerId,"S"+str(switchId), taillabel=intf.intfIp, xlabel="", label=speed+" bps", arrowhead="none")
-                if(not foundIp):
-                        switches[switchId]=intf.pointingIps
-                        switchId+=1
-                        net.edge(routerId,"S"+str(switchId), taillabel=intf.intfIp,  xlabel="", label=speed+" bps", arrowhead="none")
+                        net.edge(routerId, "S" + str(switchId), taillabel=intf.intfIp, xlabel="", label=speed + " bps",
+                                 arrowhead="none")
+                if (not foundIp):
+                    switches[switchId] = intf.pointingIps
+                    switchId += 1
+                    net.edge(routerId, "S" + str(switchId), taillabel=intf.intfIp, xlabel="", label=speed + " bps",
+                             arrowhead="none")
             else:
                 for extRouter in routersIfs.items():
                     for item in extRouter[1]:
-                        
+
                         if item.intfIp == intf.pointingIps[0]:
-                  
-                            edges.append((VectorInfo(routerId, extRouter[0], intf.intfIp, intf.pointingIps[0],item.speed)))
+                            edges.append(
+                                (VectorInfo(routerId, extRouter[0], intf.intfIp, intf.pointingIps[0], item.speed)))
     filtered_edges = []
     for edge in edges:
-        found=False
+        found = False
         for filtered_edge in filtered_edges:
-            if (VectorInfo(edge.extRouter,edge.inRouter,edge.extIp, edge.inIp,edge.speed).equals(filtered_edge)):
-                found=True
-        if(not found):
+            if (VectorInfo(edge.extRouter, edge.inRouter, edge.extIp, edge.inIp, edge.speed).equals(filtered_edge)):
+                found = True
+        if (not found):
             filtered_edges.append((edge))
     for edge in filtered_edges:
-        print(edge.inRouter,edge.extRouter,edge.inIp,edge.speed)
-        net.edge(edge.inRouter,edge.extRouter, headlabel=str(edge.inIp), taillabel=str(edge.extIp), xlabel="", label=str(edge.speed)+" bps", arrowhead="none")
-    return net,filtered_edges
+        print(edge.inRouter, edge.extRouter, edge.inIp, edge.speed)
+        net.edge(edge.inRouter, edge.extRouter, headlabel=str(edge.inIp), taillabel=str(edge.extIp), xlabel="",
+                 label=str(edge.speed) + " bps", arrowhead="none")
+    return net, filtered_edges
+
 
 class VectorInfo():
-    def __init__(self,inRouter,extRouter,inIp,extIp,speed):
-        self.inRouter=inRouter
-        self.extRouter=extRouter
-        self.inIp=inIp
-        self.extIp=extIp
-        self.speed=speed
-    def equals(self,other):
-        if(self.inRouter==other.inRouter and
-        self.extRouter==other.extRouter and
-        self.inIp==other.inIp and
-        self.extIp==other.extIp and
-        self.speed==other.speed):
+    def __init__(self, inRouter, extRouter, inIp, extIp, speed):
+        self.inRouter = inRouter
+        self.extRouter = extRouter
+        self.inIp = inIp
+        self.extIp = extIp
+        self.speed = speed
+
+    def equals(self, other):
+        if (self.inRouter == other.inRouter and
+                self.extRouter == other.extRouter and
+                self.inIp == other.inIp and
+                self.extIp == other.extIp and
+                self.speed == other.speed):
             return True
         return False
 
 
 if __name__ == "__main__":
-    #notifier = inotify.adapters.Inotify()
-    #notifier.add_watch("/etc/snmp/script/logs.txt")
-    #waitForTrap(notifier)
-    #thread = threading.Thread(target=printTrap)
-    #thread.start()
-  
-    IPs.add("11.0.5.2")  # we add our tap ip address, so it doesn't get checked
-    recursiveSearch("11.0.5.1")  # ip our tap interface is connected to
+    # notifier = inotify.adapters.Inotify()
+    # notifier.add_watch("/etc/snmp/script/logs.txt")
+    # waitForTrap(notifier)
+    # thread = threading.Thread(target=printTrap)
+    # thread.start()
+
+    IPs.add("5.0.3.2")  # we add our tap ip address, so it doesn't get checked
+    recursiveSearch("5.0.3.1")  # ip our tap interface is connected to
     print("\n 1 - POLLING ALL THE ROUTERS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     # Apartat 1 - i/f info (for every router)
     print(routersIfs)
@@ -257,13 +247,11 @@ if __name__ == "__main__":
     print(routersInfo)
 
     # Apartat 4 - Graph related code
-    net,filtered_edges =createGraph()
-    # T'he tret el render d'aqui per fer els prints d'apartats gucci, simplement està més abaix
+    net, filtered_edges = createGraph()
     print("\n 3 - CREATING ROUTE SUMMARIES >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    # Apartat 3 - Shortest paths related code (4 abans perque necessitem el filtered_edges)
-    """
-    routers = [pair[0] for pair in filtered_edges]
-    shortest_paths = dijkstra(routers)
+    # Apartat 3 - Shortest paths related code
+    routers = [(item.inIp, item.extIp) for item in filtered_edges]
+    shortest_paths = dijkstra(routersExtIps)
 
     for ip in shortest_paths:
         print(f"Shortests paths from {ip}: ")
@@ -284,10 +272,10 @@ if __name__ == "__main__":
             # elif distance == 1:
             print(f"To {target}: {' -> '.join(path)}")
         print()
-        """
+
         # distance = 0 means same router
     print("\n 4 - PLOTTING THE NETWORK (pop-up window) >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
     net.render('net.gv', view=True)
     print("\n 5 - MONITOR THE NETWORK >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     # Handled by the thread
-
